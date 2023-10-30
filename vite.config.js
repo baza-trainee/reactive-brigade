98f93720685b22d3e6018c4e34992b8ddca384ac
Baza Trainee Ukraine <132754508+baza-trainee@users.noreply.github.com>
2023-10-30 11:38:16 +0200

    fix: Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    emptyOutDir: true
-  },
   base: "/reactive-brigade/",
 });
