50e3784529c51735bbcdebf753e521472ac5e147
SergeyOdnosteblets <odnosteblets.s@gmail.com>
2023-11-11 14:16:39 +0200

    fix: fixed styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,23 @@
-import About from './components/About';
-import Footer from './components/Footer';
-import Header from './components/Header/Header';
-import Hero from './components/Hero';
-import Important from './components/Important';
-import ProgressBar from './components/ProgressBar';
-import Share from './components/Share';
+import About from "./components/About";
+import Footer from "./components/Footer";
+import Header from "./components/Header/Header";
+import Hero from "./components/Hero";
+import Important from "./components/Important";
+import ProgressBar from "./components/ProgressBar";
+import Share from "./components/Share";
 
 const App = () => {
-	return (
-		<>
-			<Header />
-			<Hero />
-			<ProgressBar />
-			<About />
-			<Important />
-			<Share />
-			<Footer />
-		</>
-	);
+  return (
+    <>
+      <Header />
+      <Hero />
+      <ProgressBar />
+      <About />
+      <Important />
+      <Share />
+      <Footer />
+    </>
+  );
 };
 
 export default App;
