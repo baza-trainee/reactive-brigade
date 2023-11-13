f6ac295283abad3b861b10b62a357dba5a850bac
Viktor Filippov <jackal3166@gmail.com>
2023-11-13 15:50:18 +0200

    Fix uah

--- src/components/Important.jsx
+++ src/components/Important.jsx
@@ -7,13 +7,7 @@ import { IconShield } from '../assets/important/icon-shield';
 const Important = () => {
 	return (
 		<section id='important'>
-			<div className='flex flex-col w-[275px] m-auto md:flex-row md:w-full sm:justify-center sm:pb-5 uppercase text-3xl font-semibold sm:text-5xl lg:text-7xl text-black'>
-				<h3>чому це&nbsp;</h3>
-				<div className='flex self-end font-semibold text-center'>
-					<h3>важливо</h3>
-					<h3 className='pl-2 text-orange'>?</h3>
-				</div>
-			</div>
+				<h3 className='text-3xl font-semibold text-center text-black sm:mb-14 mb-5 sm:text-5xl lg:text-7xl w-[80vw] mx-auto'>Чому це важливо <span className='text-orange'>?</span></h3>
 
 			<div className='flex flex-col items-center p-5 bg-orange my-7 md:hidden rounded-xl w-[90vw] mx-auto text-sm text-center min-h-[191px]'>
 				<div className='flex flex-col items-center p-3 max-w-[160px] sm:max-w-xs'>
@@ -40,35 +34,34 @@ const Important = () => {
 				className='h-[140px] bg-cover bg-center max-w-5xl md:min-h-[450px] mb-11 mx-auto'
 				style={{ backgroundImage: `url("${backgroundImageUrl}")` }}>
 				<div className='max-w-4xl'>
-					<div className='flex items-center self-center w-full p-7 sm:p-5 '>
-						<p className='font-bold text-white sm:text-2xl sm:max-w-md sm:self-start md:pl-11'>
-							Підтримавши цей благодійний проект ти наближаєш нашу
+					<div className='w-full p-7 sm:p-5 '>
+						<p className='font-bold text-white sm:text-2xl md:max-w-lg md:pl-11'>
+							Підтримавши цей благодійний проект, ти наближаєш нашу
 							армію до перемоги
 						</p>
 					</div>
 
 					<div
-						className='items-start hidden max-w-4xl mt-4 text-center text-black bg-white border-r-2 md:flex rounded-e-xl'
-						style={{ background: `rgba(255, 255, 255, 0.60)` }}>
-						<div className='flex flex-col items-center py-5 pr-8 border-r-2 border-gray-400 md:pl-16'>
-							<IconQuestion color='#F7732A' />
+						className='items-stretch hidden max-w-4xl py-5 mt-4 text-center text-black bg-white bg-opacity-60 md:flex rounded-e-xl'>
+						<div className='flex flex-col items-center pr-8 border-r border-black md:pl-16'>
+							<IconQuestion color='#F7732A'/>
 							<p className='pt-7'>
 								Неперевершена майстерність в боротьбі із ворогом
 							</p>
 						</div>
-						<div className='flex flex-col items-center px-8 py-5'>
+						<div className='flex flex-col items-center px-8 border-r border-black'>
 							<IconFire color='#F7732A' />
 							<p className='pt-7'>
 								Потужність в руках справжніх захисників
 							</p>
 						</div>
-						<div className='flex flex-col items-center px-8 py-5 border-gray-400 border-x-2'>
+						<div className='flex flex-col items-center px-8 border-r border-black'>
 							<IconShield color='#F7732A' />
 							<p className='pt-7'>
 								Завдання – знищити ворога, зберегти мир
 							</p>
 						</div>
-						<div className='flex flex-col items-center px-8 py-5'>
+						<div className='flex flex-col items-center px-8'>
 							<IconKnife color='#F7732A' />
 							<p className='pt-7'>
 								Максимізація ефективності у знищенні ворогів
